56614e052a60452a4d1123dcb48aa58c0d0f39d9
Mehdi RAFENNE <mehdi.rafenne@gmail.com>
2020-05-14 11:33:45 +0300

    fixed

---
+++ setup.sh
@@ -0,0 +1,10 @@
+export $DB_USER=postgres
+export $DB_PASSWORD=postgres
+export $DB_HOST=localhost
+export $DB_PORT=5432
+export $DB_NAME=casting_agency
+export $DB_TEST_NAME=casting_agency_test
+export $DB_URI=postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/{DB_NAME}
+export $DB_TEST_URI=postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_TEST_NAME}
+export $MOVIES_PER_PAGE=10
+export $ACTORS_PER_PAGE=10
